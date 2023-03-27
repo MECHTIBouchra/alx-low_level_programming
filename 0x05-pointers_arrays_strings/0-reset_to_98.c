79fb19fefa6e6789548c51e98fd8b151017f301e
MECHTIbouchra <mechtibouchra91@gnail.com>
2023-03-27 15:49:17 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - reset the value to 98
+  * @n: pointer of the value to be updated
+  * Return: void
+  */
+void reset_to_98(int *n)
+{
+	int *p = n;
+
+	*p =  98;
+}
+
