2e2690ad2770231538c144b44e2267ed85984a20
MECHTIbouchra <mechtibouchra91@gnail.com>
2023-04-02 20:43:41 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "main.h"
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+	int b;
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+				return (&(s[a]));
+		}
+	}
+	return (0);
+}
+
