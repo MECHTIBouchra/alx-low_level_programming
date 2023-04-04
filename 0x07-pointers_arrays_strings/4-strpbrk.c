5e3ceaa1a0bb991c77cd225d7d39131681fa5db2
MECHTIbouchra <mechtibouchra91@gnail.com>
2023-04-03 18:12:18 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,17 +1,22 @@
 #include "main.h"
-#include "stdio.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	int a;
-	int b;
+	int k;
 
-	for (a = 0; s[a]; a++)
+	while (*s)
 	{
-		for (b = 0; accept[b]; b++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[a] == accept[b])
-				return (&(s[a]));
+		if (*s == accept[k])
+		return (s);
 		}
+	s++;
 	}
-	return (0);
+	return ('\0');
 }
