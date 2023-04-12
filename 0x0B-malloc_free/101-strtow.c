b21b7dc3dd112c4cdcfb482279b4d2ace86aa873
MECHTIbouchra <mechtibouchra91@gnail.com>
2023-04-11 18:14:19 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,20 +8,19 @@
  **/
 int wrdcnt(char *s)
 {
-	int i, n = 0;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
-	}
-	n++;
-	return (n);
+int i, n = 0;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == ' ')
+{
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
+n++;
+}
+else if (i == 0)
+n++;
+}
+n++;
+return (n);
 }
 
 /**
@@ -38,7 +37,7 @@ return (NULL);
 n = wrdcnt(str);
 if (n == 1)
 return (NULL);
-w = (char **)malloc(n * sizeof(char *));
+w = (char **)malloc(n *sizeof(char *));
 if (w == NULL)
 return (NULL);
 w[n - 1] = NULL;
@@ -50,7 +49,7 @@ if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
-w[wc] = (char *)malloc(j * sizeof(char));
+w[wc] = (char *)malloc(j *sizeof(char));
 j--;
 if (w[wc] == NULL)
 {
@@ -58,14 +57,12 @@ for (k = 0; k < wc; k++)
 free(w[k]);
 free(w[n - 1]);
 free(w);
-return (NULL);
-}
+return (NULL); }
 for (l = 0; l < j; l++)
 w[wc][l] = str[i + l];
 w[wc][l] = '\0';
 wc++;
-i += j;	
-}
+i += j;	}
 else
 i++;
 }
