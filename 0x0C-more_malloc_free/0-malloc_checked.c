a8cd07cb68a1ea79d1f484aa97133c72ac8e63d2
MECHTIbouchra <mechtibouchra91@gnail.com>
2023-04-13 15:24:42 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * malloc_checked - allocate memory using malloc
+  * @b: unsigned int
+  * Return: void*
+  */
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
